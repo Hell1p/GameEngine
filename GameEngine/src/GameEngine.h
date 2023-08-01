639d928eabe6fbafdb895511e3f40f0d3c59ef62
Hell1p <106957777+Hell1p@users.noreply.github.com>
2023-08-01 20:41:57 +0300

    Init

---
+++ GameEngine/src/GameEngine.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// For use by GameEngine applications
+
+#include "GameEngine/Application.h"
+
+#pragma region EntryPoint
+
+#include "GameEngine/EntryPoint.h"
+
+#pragma endregion EntryPoint
---
+++ GameEngine/src/GameEngine/Application.cpp
@@ -0,0 +1,17 @@
+#include "Application.h"
+
+namespace GameEngine
+{
+	GameEngine::Application::Application()
+	{
+	}
+
+	GameEngine::Application::~Application()
+	{
+	}
+
+	void Application::Run()
+	{
+		while (true);
+	}
+}
